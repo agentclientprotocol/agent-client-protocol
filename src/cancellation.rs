f550f1e4000edb6498c7539c020f21c9e1df2d5c
Ben Brandt <benjamin.j.brandt@gmail.com>
2025-12-05 10:43:00 +0100

    Add cancellation capabilities

--- src/cancellation.rs
+++ src/cancellation.rs
@@ -5,6 +5,10 @@ use crate::{Meta, RequestId};
 
 pub(crate) const REQUEST_CANCEL_METHOD_NAME: &str = "$/cancelRequest";
 
+/// **UNSTABLE**
+///
+/// This capability is not part of the spec yet, and may be removed or changed at any point.
+///
 /// Notification to cancel an ongoing request.
 ///
 /// See protocol docs: [Cancellation](https://agentclientprotocol.com/protocol/cancellation)
@@ -45,4 +49,26 @@ impl CancelRequestNotification {
         self.meta = Some(meta);
         self
     }
+}
+
+/// **UNSTABLE**
+///
+/// This capability is not part of the spec yet, and may be removed or changed at any point.
+///
+/// Capabilities related to cancellation.
+///
+/// By supplying `{}` it means that general request cancellation is supported.
+///
+/// Further capabilities can be added in the future if other types of cancellation is required in the protocol.
+#[cfg(feature = "unstable_cancel_request")]
+#[derive(Default, Debug, Clone, Serialize, Deserialize, JsonSchema, PartialEq, Eq)]
+#[non_exhaustive]
+pub struct CancellationCapabilities {
+    /// The _meta property is reserved by ACP to allow clients and agents to attach additional
+    /// metadata to their interactions. Implementations MUST NOT make assumptions about values at
+    /// these keys.
+    ///
+    /// See protocol docs: [Extensibility](https://agentclientprotocol.com/protocol/extensibility)
+    #[serde(skip_serializing_if = "Option::is_none", rename = "_meta")]
+    pub meta: Option<Meta>,
 }
